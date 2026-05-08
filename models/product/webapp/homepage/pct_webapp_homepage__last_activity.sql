8ba1ffc27ae266cb7d8e363dd810c21a5cba03c9
Etienne LANCELLE <etiennelancelle@outlook.fr>
2026-05-08 23:03:03 +0200

    feat: try

--- models/product/webapp/homepage/pct_webapp_homepage__last_activity.sql
+++ models/product/webapp/homepage/pct_webapp_homepage__last_activity.sql
@@ -15,7 +15,8 @@ WITH last_activity AS (
         performance.duration_s,
         performance.avg_pace_min_per_km,
         performance.avg_hr_bpm,
-        performance.elevation_gain_m
+        performance.elevation_gain_m,
+        timeseries
     FROM {{ ref('svc_hub__master_running_activities') }}
     ORDER BY activity_date DESC, start_time_local DESC
     LIMIT 1
@@ -29,7 +30,19 @@ SELECT
     cast(round(avg_pace_min_per_km * 60) AS int64) AS pace_seconds_per_km,
     cast(round(avg_hr_bpm) AS int64) AS avg_heart_rate_bpm,
     cast(round(elevation_gain_m) AS int64) AS elevation_gain_m,
-    cast(NULL AS string) AS polyline,
+    (
+        SELECT
+            concat(
+                '[',
+                string_agg(
+                    concat('[', cast(ts.latitude AS string), ',', cast(ts.longitude AS string), ']'),
+                    ','
+                ),
+                ']'
+            )
+        FROM unnest(timeseries) AS ts
+        WHERE ts.latitude IS NOT NULL AND ts.longitude IS NOT NULL
+    ) AS polyline,
     concat(
         cast(extract(DAY FROM activity_date) AS string),
         ' ',
