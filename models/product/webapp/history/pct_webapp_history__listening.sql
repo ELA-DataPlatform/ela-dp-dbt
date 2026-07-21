bc21e19d4342efa8712be515d1c4a9bae86ec79a
Edgar <edgar@local>
2026-07-21 13:18:34 +0000

    feat: add listening history data product

---
+++ models/product/webapp/history/pct_webapp_history__listening.sql
@@ -0,0 +1,30 @@
+{{
+    config(
+        materialized='table',
+        tags=['spotify']
+    )
+}}
+
+WITH primary_artist AS (
+    SELECT
+        bta.track_id,
+        a.artist_name
+    FROM {{ ref('svc_hub__bridge_track_artist') }} AS bta
+    INNER JOIN {{ ref('svc_hub__ref_artist') }} AS a ON bta.artist_id = a.artist_id
+    WHERE bta.artist_position = 0
+)
+
+SELECT
+    fp.played_at,
+    fp.track_id,
+    t.track_name,
+    pa.artist_name,
+    fp.album_id,
+    al.album_name,
+    al.album_image_url,
+    t.duration_ms
+FROM {{ ref('svc_hub__fact_played') }} AS fp
+LEFT JOIN {{ ref('svc_hub__ref_track') }} AS t ON fp.track_id = t.track_id
+LEFT JOIN {{ ref('svc_hub__ref_album') }} AS al ON fp.album_id = al.album_id
+LEFT JOIN primary_artist AS pa ON fp.track_id = pa.track_id
+
